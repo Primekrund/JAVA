88d2404c4d3edfef5197d24cf9e0d7d7a686741c
Primekrund <1414826853@qq.com>
2018-09-13 16:20:44 +0800

    首次进行版本控制，要纪念一下自己菜鸟吗？

--- hotelmanage/src/test/java/com/krund/hotel/manage/service/BaseTest.java
+++ hotelmanage/src/test/java/com/krund/hotel/manage/service/BaseTest.java
@@ -3,7 +3,6 @@ package com.krund.hotel.manage.service;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
-
 /**
  * 单元测试基类
  * @author Zhang Ziming
